9e9389c502435382cdb49844787bbac0e0b79ed9
Liam Canetti <git@liamcanetti.co.uk>
2024-03-19 18:52:45 +0000

    chore: minor UI for mobile

--- src/App.tsx
+++ src/App.tsx
@@ -3,20 +3,30 @@ import { DotLottiePlayer } from "@dotlottie/react-player";
 import "@dotlottie/react-player/dist/index.css";
 
 export const App = () => {
-	return (
-		<>
-			<h1>Lottie Tester</h1>
-			<div className="players">
-				<div>
-					<DotLottiePlayer src="Fireworks.lottie" autoplay loop style={{ width: 350, height: 350 }} />
+  return (
+    <>
+      <h1>Lottie Tester</h1>
+      <div className="players">
+        <div>
+          <DotLottiePlayer
+            src="Fireworks.lottie"
+            autoplay
+            loop
+            style={{ width: 350, height: 350 }}
+          />
 
-					<h2>DotLottie file</h2>
-				</div>
-				<div>
-					<DotLottiePlayer src="Fireworks.json" autoplay loop style={{ width: 350, height: 350 }} />
-					<h2>Lottie Json file</h2>
-				</div>
-			</div>
-		</>
-	);
+          <h2>DotLottie file (5.2Kb)</h2>
+        </div>
+        <div>
+          <DotLottiePlayer
+            src="Fireworks.json"
+            autoplay
+            loop
+            style={{ width: 350, height: 350 }}
+          />
+          <h2>Original JSON file (44kb)</h2>
+        </div>
+      </div>
+    </>
+  );
 };
